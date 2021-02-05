951b3c8c534f6c0fc285fe0e4f10e440a24af4a5
lirdai <lirdaiiub@gmail.com>
2021-02-05 02:08:12 -0500

    build manually copy into backend

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,7 @@
 FROM node:15.5.1-alpine3.10
 
 # cd 
-WORKDIR /usr/src/app/frontend
-
-# install dependencies
-COPY frontend/package*.json ./
-RUN npm ci --only=production --verbose
-COPY frontend/ ./
-
-RUN npm run build
 WORKDIR /usr/src/app/
-
-# mv --> inside app
-RUN mv frontend/build . && rm -r frontend
 
 COPY backend/package*.json ./
 RUN npm ci --only=production --verbose
--- backend/mongo.js
+++ backend/mongo.js
@@ -270,6 +270,9 @@ const resolvers = {
             try {
                 const s3 = new AWS.S3({
                     apiVersion: "2006-03-01",
+                    region: process.env.AWS_REGION,
+                    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, 
                     params: { Bucket: bucket }
                 })
                 
