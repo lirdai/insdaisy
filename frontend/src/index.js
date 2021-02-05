98fc0a78a2d1950b1f733ebf929c9ec116dc52a8
lirdai <lirdaiiub@gmail.com>
2021-02-05 15:23:13 -0500

    uri change for production

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -51,11 +51,11 @@ const authLink = setContext((_, { headers }) => {
 })
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'http://insdaisy.daisywebdev.com/graphql',
 })
 
 const wsLink = new WebSocketLink({
-  uri: `ws://localhost:4000/graphql-ws`,
+  uri: `ws://insdaisy.daisywebdev.com/graphql-ws`,
   options: {
     reconnect: true
   }
